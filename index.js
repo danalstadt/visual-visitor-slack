396146ba725d5ee35225de86898997f6e2363807
Dan Alstadt <danalstadt@gmail.com>
2016-03-02 11:56:27 -0600

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+});
