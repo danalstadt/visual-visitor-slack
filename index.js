e3e9674219c4de3558fae40de15a9cb3dfe8dde0
Dan Alstadt <danalstadt@gmail.com>
2016-03-02 12:31:02 -0600

    better formatting

--- index.js
+++ index.js
@@ -10,11 +10,14 @@ app.post('/', function (req, res) {
     console.log(req.body);
 
     var message = format(
-        'Someone from <{c_visitor_company_domain}|{c_visitor_company_name}> ' +
-        'in {c_visitor_company_city}, {c_visitor_company_state}, {c_visitor_company_country} ' +
+        'Someone from {company_name} ' +
+        'in {c_visitor_company_city}, {c_visitor_company_state}, {c_visitor_company_country}, ' +
         'has visited {c_visitor_current_page_request_count} {page_plural} on the site.',
-        _(req.body).clone().assign({
-            page_plural: parseInt(req.body.c_visitor_current_page_request_count) > 1 ? 'pages' : 'page'
+        _(req.body).assign({
+            page_plural: parseInt(req.body.c_visitor_current_page_request_count) > 1 ? 'pages' : 'page',
+            company_name: req.body.c_visitor_company_domain == 'no domain name found' ?
+                req.body.c_visitor_company_name :
+                '<{c_visitor_company_domain}|{c_visitor_company_name}>'.format(req.body)
         }).value()
     );
 
