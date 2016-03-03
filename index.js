85b5ac66f4c00a946cc25a273fff3746fbf985da
Dan Alstadt <danalstadt@gmail.com>
2016-03-03 13:39:29 -0600

    adding startsWith polyfill

--- index.js
+++ index.js
@@ -50,3 +50,10 @@ app.post('/', function (req, res) {
 });
 
 app.listen(conf.port);
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+}
